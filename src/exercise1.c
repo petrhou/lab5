1f65c3d98ad9ade4b3f7c08124c770290e24354c
Королев Пётр Алексеевич ИП - 516 <korolevp712@gmail.com>
2025-10-14 16:03:20 +0700

    FIX

--- src/exercise1.c
+++ src/exercise1.c
@@ -2,10 +2,16 @@
 
 int main() {
     double arr[10], sum = 0;
+    
     for (int i = 0; i < 10; i++) {
         scanf("%lf", &arr[i]);
         sum += arr[i];
     }
-    printf("%.2f\n", sum / 10);
+    
+    // Округление без math.h
+    double average = sum / 10;
+    average = (int)(average * 100 + 0.5) / 100.0;
+    printf("%.2f\n", average);
+    
     return 0;
 }
