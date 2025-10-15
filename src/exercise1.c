0ddfc4c4e9f78ee37758bfefd6885713f5f2675f
Королев Пётр Алексеевич ИП - 516 <korolevp712@gmail.com>
2025-10-15 11:51:27 +0700

    FINAL_FIX

--- src/exercise1.c
+++ src/exercise1.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <math.h>
 
 int main() {
     double arr[10], sum = 0;
@@ -9,16 +8,7 @@ int main() {
         sum += arr[i];
     }
     
-    // Используем точное округление
-    double average = sum / 10;
+    printf("%.2f\n", sum / 10);
     
-    // Прибавляем очень маленькое значение для правильного округления
-    if (average > 0) {
-        average += 0.0000001;
-    } else {
-        average -= 0.0000001;
-    }
-    
-    printf("%.2f\n", average);
     return 0;
 }
