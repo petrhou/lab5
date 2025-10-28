1b211f7a9155d05d206a9a581d31c8537f776940
Королев Пётр Алексеевич ИП - 516 <korolevp712@gmail.com>
2025-10-28 13:30:00 +0700

    test1

--- src/array_function.c
+++ src/array_function.c
@@ -10,6 +10,24 @@ void join_int_arrays(int *src1, size_t size_src1, int *src2, size_t size_src2, i
 }
 
 void join_and_sort_int_arrays(int* src1, size_t size_src1, int* src2, size_t size_src2, int* dest) {
+    if (size_src1 == 0 && size_src2 == 0) {
+        return;
+    }
+
+    if (size_src1 == 0) {
+        for (size_t i = 0; i < size_src2; i++) {
+            dest[i] = src2[i];
+        }
+        return;
+    }
+
+    if (size_src2 == 0) {
+        for(size_t i = 0; i < size_src1; i++) {
+            dest[i] = src1[i];
+        }
+        return;
+    }
+
     size_t i = 0, j = 0, k = 0;
 
     while (i < size_src1 && j < size_src2) {
@@ -42,4 +60,3 @@ void get_min_and_max_from_int_array(int* src, size_t size, int* min, int* max) {
         }
     }
 }
-
